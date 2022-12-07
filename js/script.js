606aaecea5bcc2eeb7f3f9aa3b50708ddeb096bb
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-12-07 19:37:21 +0000

    angle check

--- js/script.js
+++ js/script.js
@@ -25,8 +25,13 @@ function update() {
   const b = parseFloat(document.getElementById("lenghts2").value)
   const c = parseFloat(document.getElementById("lenghts3").value)
 
+  const angleA = Math.acos((b**2 + c**2 - a**2) / (2 * b * c)) * (180/Math.PI)
+  const angleB = Math.acos((c**2 + a**2 - b**2) / (2 * c * a)) * (180/Math.PI)
+  const angleC = Math.acos((a**2 + b**2 - c**2) / (2 * a * b)) * (180/Math.PI)
+  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+
   // output
-  if (a + b <= c || b + c <= a || c + a <= b || a <= 0 || b <= 0 || c <= 0) {
+  if ((a + b <= c || b + c <= a || c + a <= b || a <= 0 || b <= 0 || c <= 0) || ((Math.round(sumOfAngles)) != 180)) {
     console.log(1)
     document.getElementById("answer").innerHTML = "It is not a triangle"
   } else {
